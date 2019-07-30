c4ce703080a4294e0464def429ced1e30a1af7a0
mysoulCc <15993013693@163.com>
2019-07-30 23:59:54 +0800

    更新接口数据

--- my_project_bmc/src/mock.js
+++ my_project_bmc/src/mock.js
@@ -9,10 +9,12 @@ Mock.mock('api/loginDate', 'get',(req,res)=>{
 })
 Mock.mock('api/homeDate', 'get',(req,res)=>{
     return{      
-        'ipv4_ip':Random.ip(),
-        'ipv4_Subnet_mask':Random.ip(),
-        'Client_Number':Random.natural( 1, 10 ),
-        'SIM_Status':Random.word(),
-        'Client_Number': Random.natural( 1, 5 )
+        'LAN':{
+            'ipv4_ip':Random.ip(),
+            'ipv4_Subnet_mask':Random.ip(),
+            'Client_Number':Random.natural( 1, 10 ),
+            'SIM_Status':Random.word(),
+            'Client_Number': Random.natural( 1, 5 )
+        }
     }
 })
