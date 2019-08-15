4a4b576f97f1042ed97b5d07a8cff15c374977fb
mysoulCc <15993013693@163.com>
2019-08-15 16:01:26 +0800

    使用elementui框架

--- my_project_bmc/src/components/index/Wizard/Wizard_WAN.vue
+++ my_project_bmc/src/components/index/Wizard/Wizard_WAN.vue
@@ -3,17 +3,42 @@
         <span>Please consult with service provider for these settings. If not sure, leave them with default value.</span>
         <h3><font>WAN Configuration</font></h3>  
         <div class="wizardContentClass">
-           
+           <el-table
+            :data="tableData"
+            style="width: 100%">
+                <el-table-column
+                    prop="Type"
+                    label="Type"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="apn"
+                    label="APN"
+                    width="180">
+                </el-table-column>
+            
+            </el-table>
         </div>
     </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            msg:'这是wizard第2页'
+     data() {
+        return {
+          tableData: [{
+            Type: 'IPv4 Data Connection',
+            apn: ''
+          }, {
+            Type: 'IPv6 Data Connection',
+            apn: ''
+          }]
         }
-    }
+      },
+      created(){
+         for (let index of this.tableData) {
+                index.apn = '3gnet'                 
+         }
+      }
 }
 </script>
 <style scoped>
@@ -21,6 +46,19 @@ export default {
         display: block;
         padding-bottom: 10px;
         color: #0464BB;
-    }
+    } 
+    
 </style>
+<style>
+     #Wizard_WAN .cell{
+        color: #0464BB;
+    }
+    #Wizard_WAN .has-gutter th{
+        background-color: #aabbc5;
+        color: #fff;
+        padding: 2px;
+    }
+   
+</style>
+
 
--- my_project_bmc/src/main.js
+++ my_project_bmc/src/main.js
@@ -6,8 +6,11 @@ import $ from 'jquery'
 import App from './App'
 import router from './router'
 import store from './store/store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import Confirm from './components/Confirm'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 require('./mock')
 Vue.prototype.axios = axios;
