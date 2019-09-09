54f387fd9aceca33d24aa189be92e8bc0bb8c2b0
mysoulCc <15993013693@163.com>
2019-09-09 17:55:12 +0800

    更新ui布局框架

--- my_project_bmc/src/components/index/Security/NAT.vue
+++ my_project_bmc/src/components/index/Security/NAT.vue
@@ -1,6 +1,20 @@
 <template>
-    <div id="NAT">
-        {{msg}}
+    <div class="Paging-content-style">
+        <div class="SubpageContent-Content">
+            <el-row :gutter="20">
+                <el-col>
+                    <div class="ContentTitle">
+                        <span>NAT</span>
+                        <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+                            <el-tab-pane label="Configure" name="first"></el-tab-pane>                       
+                        </el-tabs>
+                    </div>
+                </el-col>
+            </el-row>
+            <div v-if="LabelContent === 'first'">
+                {{msg}}
+            </div>          
+        </div>
     </div>
 </template>
 <script>
@@ -8,9 +22,45 @@ export default {
     name:'NATLink',
     data(){
         return{
+            activeName:'first',
+            LabelContent: 'first',
             msg:'这是NAT页面'
         }
+    },
+    methods:{
+        // 标签选择事件
+        handleClick(tab, event){
+           this.LabelContent = tab.paneName;
+        },
     }
 }
 </script>
+<style scoped>
+    .el-row {
+        margin-bottom: 20px;
+        &:last-child {
+        margin-bottom: 0;
+        }
+    }
+    .el-col {
+        border-radius: 4px;
+    }
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+    .row-bg {
+        padding: 10px 0;
+        background-color: #f9fafc;
+    }
 
+</style>
+<style>
+    .Paging-content-style .has-gutter th{
+        background-color: #aabbc5;
+        padding: 2px;
+    }
+    .Paging-content-style .has-gutter th div{      
+        color: #fff;
+    }
+</style>
